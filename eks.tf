da2482180595f743a360427610caeb68fd895ae9
Idan Brody <idan@projectcircle.co.il>
2024-08-29 16:08:04 +0300

    wip

--- eks.tf
+++ eks.tf
@@ -257,9 +257,6 @@ module "eks" {
 
 #############################################################################################################################################
 
-
-
-
 # EBS CSI Driver IRSA 
 module "irsa-ebs-csi" {
   source                = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
@@ -272,39 +269,4 @@ module "irsa-ebs-csi" {
       namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
     }
   }
-}
-
-# Remove non encrypted default storage class
-resource "kubernetes_annotations" "default_storageclass" {
-  api_version = "storage.k8s.io/v1"
-  kind        = "StorageClass"
-  force       = "true"
-  metadata {
-    name = data.kubernetes_storage_class.name.metadata[0].name
-  }
-  annotations = {
-    "storageclass.kubernetes.io/is-default-class" = "false"
-  }
-  depends_on = [module.eks]
-}
-
-resource "kubernetes_storage_class" "ebs_sc_gp3" {
-  storage_provisioner    = "ebs.csi.aws.com"
-  reclaim_policy         = var.storage_reclaim_policy
-  allow_volume_expansion = true
-  volume_binding_mode    = "WaitForFirstConsumer"
-  parameters = {
-    "csi.storage.k8s.io/fstype" = "ext4"
-    encrypted                   = "true"
-    type                        = "gp3"
-    tagSpecification_1          = "Name={{ .PVCNamespace }}/{{ .PVCName }}"
-    tagSpecification_2          = "Namespace={{ .PVCNamespace }}"
-  }
-  metadata {
-    name = "ebs-sc-gp3"
-    annotations = {
-      "storageclass.kubernetes.io/is-default-class" = "true"
-    }
-  }
-  depends_on = [kubernetes_annotations.default_storageclass]
 }
