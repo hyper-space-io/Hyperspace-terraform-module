ae5e4a7c588cd1b19bb1c31a6b908f4a89863b9f
Idan Brody <idan@projectcircle.co.il>
2024-09-17 11:54:14 +0300

    resturcture

---
+++ Infra-module/outputs.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "private_subnet_ids" {
+  description = "IDs of the private subnets"
+  value       = module.vpc.private_subnets
+}
+
+output "public_subnet_ids" {
+  description = "IDs of the public subnets"
+  value       = module.vpc.public_subnets
+}
+
+output "s3_endpoint_id" {
+  description = "The ID of the S3 VPC endpoint"
+  value       = module.endpoints.endpoints["s3"].vpc_endpoint_id
+}
