5950cf3b552c6a1d0dbee51a90f9fefc14827b66
Idan Brody <idan@projectcircle.co.il>
2024-09-17 17:00:09 +0300

    added storage class

--- app-module/main.tf
+++ app-module/main.tf
@@ -42,3 +42,43 @@ provider "helm" {
     token                  = data.terraform_remote_state.infra.outputs.eks_token
   }
 }
+
+
+data "kubernetes_storage_class" "name" {
+  metadata { name = "gp2" }
+}
+
+# Remove non encrypted default storage class
+resource "kubernetes_annotations" "default_storageclass" {
+  api_version = "storage.k8s.io/v1"
+  kind        = "StorageClass"
+  force       = "true"
+
+  metadata {
+    name = data.kubernetes_storage_class.name.metadata[0].name
+  }
+  annotations = {
+    "storageclass.kubernetes.io/is-default-class" = "false"
+  }
+}
+
+resource "kubernetes_storage_class" "ebs_sc_gp3" {
+  metadata {
+    name = "ebs-sc-gp3"
+    annotations = {
+      "storageclass.kubernetes.io/is-default-class" = "true"
+    }
+  }
+  storage_provisioner = "ebs.csi.aws.com"
+#   reclaim_policy      = var.storage_reclaim_policy
+  parameters = {
+    "csi.storage.k8s.io/fstype" = "ext4"
+    encrypted                   = "true"
+    type                        = "gp3"
+    tagSpecification_1          = "Name={{ .PVCNamespace }}/{{ .PVCName }}"
+    tagSpecification_2          = "Namespace={{ .PVCNamespace }}"
+  }
+  allow_volume_expansion = true
+  volume_binding_mode    = "WaitForFirstConsumer"
+  depends_on             = [kubernetes_annotations.default_storageclass]
+}
