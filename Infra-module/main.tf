cafd5239ab5d953c9cf364d46956b51380b6ca52
Idan Brody <idan@projectcircle.co.il>
2024-09-18 11:42:39 +0300

    removed redundant sec group

--- Infra-module/main.tf
+++ Infra-module/main.tf
@@ -6,28 +6,3 @@ resource "aws_iam_policy" "policies" {
   description = each.value.description
   policy      = each.value.policy
 }
-
-# TFC AGENT
-resource "aws_instance" "tfc_agent" {
-  ami                    = data.aws_ami.amazon_linux_2.id
-  instance_type          = "t3.medium"
-  subnet_id              = module.vpc.private_subnets[0]
-  vpc_security_group_ids = [aws_security_group.tfc_agent_sg.id]
-  user_data = templatefile("user_data.sh.tpl", {
-    tfc_agent_token = var.tfc_agent_token
-  })
-  tags = {
-    Name = "tfc-agent"
-  }
-}
-resource "aws_security_group" "tfc_agent_sg" {
-  name        = "tfc-agent-sg"
-  description = "Security group for Terraform Cloud Agent"
-  vpc_id      = module.vpc.vpc_id
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
