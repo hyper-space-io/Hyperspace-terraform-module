f53e8bb6e2a590bba4f22f63575723f4cb17b5ab
Idan Brody <idan@projectcircle.co.il>
2024-09-18 13:21:48 +0300

    revert

--- app-module/locals.tf
+++ app-module/locals.tf
@@ -1,12 +1,8 @@
 locals {
-  infra_outputs = data.terraform_remote_state.infra.outputs
-  # aws_region = data.terraform_remote_state.infra.outputs.aws_region
-  # create_eks = data.terraform_remote_state.infra.outputs.create_eks
-  # eks_module = data.terraform_remote_state.infra.outputs.eks_cluster
-  aws_region = infra_outputs.aws_region
-  create_eks = infra_outputs.create_eks
-  eks_module = infra_outputs.eks_cluster
-  vpc_module = infra_outputs.vpc
+  aws_region = data.terraform_remote_state.infra.outputs.aws_region
+  create_eks = data.terraform_remote_state.infra.outputs.create_eks
+  eks_module = data.terraform_remote_state.infra.outputs.eks_cluster
+  vpc_module = data.terraform_remote_state.infra.outputs.vpc
   alb_values = <<EOT
   vpcId: ${local.vpc_module.vpc_id}
   region: ${local.aws_region}
