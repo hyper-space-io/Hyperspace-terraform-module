ecde518986892926cad5789824f8c92ed66aebca
Idan Brody <idan@projectcircle.co.il>
2024-09-18 14:58:34 +0300

    Removed redundant local

--- app-module/locals.tf
+++ app-module/locals.tf
@@ -1,6 +1,5 @@
 locals {
   aws_region = data.terraform_remote_state.infra.outputs.aws_region
-  create_eks = data.terraform_remote_state.infra.outputs.create_eks
   eks_module = data.terraform_remote_state.infra.outputs.eks_cluster
   vpc_module = data.terraform_remote_state.infra.outputs.vpc
   alb_values = <<EOT
