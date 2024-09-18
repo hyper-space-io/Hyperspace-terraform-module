548c5d74f059d686335f332462b9091d0bd52205
Idan Brody <idan@projectcircle.co.il>
2024-09-18 13:39:23 +0300

    hotfix leftovers

--- Infra-module/tfc_agent.tf
+++ Infra-module/tfc_agent.tf
@@ -4,7 +4,6 @@ resource "aws_instance" "tfc_agent" {
   instance_type          = "t3.medium"
   subnet_id              = module.vpc.private_subnets[0]
   vpc_security_group_ids = [aws_security_group.tfc_agent_sg.id]
-  iam_instance_profile   = aws_iam_instance_profile.tfc_agent_instance_profile.name
   user_data = templatefile("user_data.sh.tpl", {
     tfc_agent_token = var.tfc_agent_token
   })
