c528389f4bd57b1c260620c9321b48122ac7b97b
Idan Brody <idan@projectcircle.co.il>
2024-09-18 13:19:59 +0300

    Refactor outputs usage

--- app-module/locals.tf
+++ app-module/locals.tf
@@ -1,9 +1,14 @@
 locals {
-  aws_region = data.terraform_remote_state.infra.outputs.aws_region
-  create_eks = data.terraform_remote_state.infra.outputs.create_eks
-  eks_module = data.terraform_remote_state.infra.outputs.eks_cluster
+  infra_outputs = data.terraform_remote_state.infra.outputs
+  # aws_region = data.terraform_remote_state.infra.outputs.aws_region
+  # create_eks = data.terraform_remote_state.infra.outputs.create_eks
+  # eks_module = data.terraform_remote_state.infra.outputs.eks_cluster
+  aws_region = infra_outputs.aws_region
+  create_eks = infra_outputs.create_eks
+  eks_module = infra_outputs.eks_cluster
+  vpc_module = infra_outputs.vpc
   alb_values = <<EOT
-  vpcId: ${module.vpc.vpc_id}
+  vpcId: ${local.vpc_module.vpc_id}
   region: ${local.aws_region}
   EOT
 }
