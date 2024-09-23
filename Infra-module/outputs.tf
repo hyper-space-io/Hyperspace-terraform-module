36fec8b0f1c65f7d7801030941069fc69cbcf6ce
Idan Brody <idan@projectcircle.co.il>
2024-09-23 09:23:27 +0300

    removed redundant outputs

--- Infra-module/outputs.tf
+++ Infra-module/outputs.tf
@@ -1,23 +1,3 @@
-output "vpc_id" {
-  description = "The unique identifier (ID) of the VPC where resources are deployed."
-  value       = module.vpc.vpc_id
-}
-
-output "private_subnet_ids" {
-  description = "List of IDs of the private subnets within the VPC. These subnets are used for internal resources that do not require direct access from the internet."
-  value       = module.vpc.private_subnets
-}
-
-output "public_subnet_ids" {
-  description = "List of IDs of the public subnets within the VPC. These subnets are used for resources that require direct access to the internet, such as NAT gateways or bastion hosts."
-  value       = module.vpc.public_subnets
-}
-
-output "s3_endpoint_id" {
-  description = "The ID of the VPC endpoint used for connecting to Amazon S3 privately, without needing to traverse the internet."
-  value       = module.endpoints.endpoints["s3"].id
-}
-
 output "aws_region" {
   value       = var.aws_region
   description = "The AWS region where the VPC and all associated resources are deployed."
