ba50ee4c628283490e4832e20c905981cf8bf426
Idan Brody <idan@projectcircle.co.il>
2024-10-07 17:00:50 +0300

    Added try to all S3 block

--- Infra-module/S3.tf
+++ Infra-module/S3.tf
@@ -18,17 +18,17 @@ module "s3_buckets" {
   bucket        = "hyperspace-${var.environment}-${each.key}-${random_string.random[each.key].result}"
   tags          = var.tags
   acl           = null
-  policy        = each.value.existing_policy_arn
-  attach_policy = each.value.existing_policy_arn != null ? true : false
-  force_destroy = each.value.force_destroy == true ? true : false
+  policy        = try(each.value.existing_policy_arn, null)
+  attach_policy = try(each.value.existing_policy_arn, null) != null
+  force_destroy = try(each.value.force_destroy, false)
   versioning = {
-    enabled = each.value.versioning
+    enabled = try(each.value.versioning, false)
   }
-  attach_lb_log_delivery_policy  = each.value.attach_lb_log_delivery_policy != true ? false : true
-  attach_elb_log_delivery_policy = each.value.attach_elb_log_delivery_policy != true ? false : true
-  ignore_public_acls             = each.value.ignore_public_acls != null ? each.value.ignore_public_acls : false
-  block_public_policy            = each.value.block_public_policy != null ? each.value.block_public_policy : false
-  block_public_acls              = each.value.block_public_acls != null ? each.value.block_public_acls : false
+  attach_lb_log_delivery_policy  = try(each.value.attach_lb_log_delivery_policy, false)
+  attach_elb_log_delivery_policy = try(each.value.attach_elb_log_delivery_policy, false)
+  ignore_public_acls             = try(each.value.ignore_public_acls, false)
+  block_public_policy            = try(each.value.block_public_policy, false)
+  block_public_acls              = try(each.value.block_public_acls, false)
 }
 
 resource "random_string" "random" {
