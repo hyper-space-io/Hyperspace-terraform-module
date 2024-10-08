e22648f1499e628201a47f721f097ab875525dbf
Idan Brody <idan@projectcircle.co.il>
2024-10-08 14:44:08 +0300

    Changed logic in acm creation

--- app-module/acm.tf
+++ app-module/acm.tf
@@ -1,26 +1,26 @@
 locals {
   create_acm = var.domain_name != "" ? true : false
   acm_config = {
-    external_acm = {
+    external_acm = var.create_public_zone && var.domain_name != "" ? {
       domain_name = var.domain_name
       subject_alternative_names = [
         "*.${var.domain_name}",
       ]
       create_certificate = local.create_acm
-    },
-    internal_acm = {
+    } : null,
+    internal_acm = local.internal_domain_name != "" ? {
       domain_name = local.internal_domain_name
       subject_alternative_names = [
         "*.${local.internal_domain_name}",
       ]
       create_certificate = local.create_acm
-    }
+    } : null
   }
 }
 module "acm" {
   source                    = "terraform-aws-modules/acm/aws"
   version                   = "5.0.1"
-  for_each                  = local.acm_config
+  for_each                  = { for k, v in local.acm_config : k => v if v != null }
   create_certificate        = each.value.create_certificate
   domain_name               = each.value.domain_name
   zone_id                   = each.key == "external_acm" && var.create_public_zone ? module.zones["external"].route53_zone_zone_id : module.zones.route53_zone_zone_id["internal"]
