7f815cb45bc2485cdd5f265f7ccc27f62adb95dc
Idan Brody <idan@projectcircle.co.il>
2024-10-14 11:35:21 +0300

    revert PR

--- app-module/data.tf
+++ app-module/data.tf
@@ -13,21 +13,21 @@ data "kubernetes_storage_class" "name" {
   metadata { name = "gp2" }
 }
 
-data "kubernetes_ingress_v1" "ingress" {
-  metadata {
-    name      = "external-ingress"
-    namespace = "ingress"
-  }
-  depends_on = [kubernetes_ingress_v1.nginx_ingress["external"]]
-}
+# data "kubernetes_ingress_v1" "ingress" {
+#   metadata {
+#     name      = "external-ingress"
+#     namespace = "ingress"
+#   }
+#   depends_on = [kubernetes_ingress_v1.nginx_ingress["external"]]
+# }
 
-data "kubernetes_ingress_v1" "internal_ingress" {
-  metadata {
-    name      = "internal-ingress"
-    namespace = "ingress"
-  }
-  depends_on = [kubernetes_ingress_v1.nginx_ingress["internal"]]
-}
+# data "kubernetes_ingress_v1" "internal_ingress" {
+#   metadata {
+#     name      = "internal-ingress"
+#     namespace = "ingress"
+#   }
+#   depends_on = [kubernetes_ingress_v1.nginx_ingress["internal"]]
+# }
 
 data "aws_eks_cluster_auth" "eks" {
   name = local.eks_module.cluster_name
