be1ba0c006311661cd2d76421587edda62ab7480
Idan Brody <idan@projectcircle.co.il>
2024-10-14 13:23:05 +0300

    revert PR

--- app-module/acm.tf
+++ app-module/acm.tf
@@ -1,32 +1,32 @@
-locals {
-  create_acm = var.domain_name != "" ? true : false
-  acm_config = {
-    external_acm = var.create_public_zone && local.public_domain_name != "" ? {
-      domain_name = local.public_domain_name
-      subject_alternative_names = [
-        "*.${local.public_domain_name}",
-      ]
-      create_certificate = local.create_acm
-    } : null,
-    internal_acm = local.internal_domain_name != "" ? {
-      domain_name = local.internal_domain_name
-      subject_alternative_names = [
-        # "${var.domain_name}",
-        "*.${local.internal_domain_name}",
-      ]
-      create_certificate = local.create_acm
-    } : null
-  }
-}
-module "acm" {
-  source                    = "terraform-aws-modules/acm/aws"
-  version                   = "5.0.1"
-  for_each                  = { for k, v in local.acm_config : k => v if v != null }
-  create_certificate        = each.value.create_certificate
-  domain_name               = each.value.domain_name
-  subject_alternative_names = each.value.subject_alternative_names
-  tags                      = local.tags
-  validation_method         = "DNS"
-  wait_for_validation       = true
-  create_route53_records    = false
-}
+# locals {
+#   create_acm = var.domain_name != "" ? true : false
+#   acm_config = {
+#     external_acm = var.create_public_zone && local.public_domain_name != "" ? {
+#       domain_name = local.public_domain_name
+#       subject_alternative_names = [
+#         "*.${local.public_domain_name}",
+#       ]
+#       create_certificate = local.create_acm
+#     } : null,
+#     internal_acm = local.internal_domain_name != "" ? {
+#       domain_name = local.internal_domain_name
+#       subject_alternative_names = [
+#         # "${var.domain_name}",
+#         "*.${local.internal_domain_name}",
+#       ]
+#       create_certificate = local.create_acm
+#     } : null
+#   }
+# }
+# module "acm" {
+#   source                    = "terraform-aws-modules/acm/aws"
+#   version                   = "5.0.1"
+#   for_each                  = { for k, v in local.acm_config : k => v if v != null }
+#   create_certificate        = each.value.create_certificate
+#   domain_name               = each.value.domain_name
+#   subject_alternative_names = each.value.subject_alternative_names
+#   tags                      = local.tags
+#   validation_method         = "DNS"
+#   wait_for_validation       = true
+#   create_route53_records    = false
+# }
