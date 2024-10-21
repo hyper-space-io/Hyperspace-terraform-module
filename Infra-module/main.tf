5306f2f0ab9cb356f9c22e7dbbeaec7a3355e52a
Idan Brody <idan@projectcircle.co.il>
2024-10-21 09:33:02 +0300

    Changed variable name in argocd tpl

--- Infra-module/main.tf
+++ Infra-module/main.tf
@@ -10,7 +10,7 @@ resource "aws_iam_policy" "policies" {
 module "iam_iam-assumable-role-with-oidc" {
   source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version                       = "~> 5.46.0"
-  for_each                      = { for k, v in local.iam_policies : k => v if lookup(v , "create_assumable_role", false) == true }
+  for_each                      = { for k, v in local.iam_policies : k => v if lookup(v, "create_assumable_role", false) == true }
   create_role                   = true
   role_name                     = each.value.name
   provider_url                  = module.eks.cluster_oidc_issuer_url
--- app-module/argocd.tf
+++ app-module/argocd.tf
@@ -1,13 +1,13 @@
 locals {
   argocd_values = templatefile("${path.module}/argocd_values.tftpl", {
-    dex_enabled                = length(var.dex_connectors) > 0
-    domain                     = local.internal_domain_name
-    dex_connectors             = var.dex_connectors
-    rbac_policy_default        = var.argocd_rbac_policy_default
-    rbac_policy_rules          = var.argocd_rbac_policy_rules
-    server_autoscaling_enabled = var.enable_ha_argocd
-    ingress_enabled            = local.eks_exists
-    ingress_class              = "nginx-internal"
+    dex_enabled              = length(var.dex_connectors) > 0
+    domain                   = local.internal_domain_name
+    dex_connectors           = var.dex_connectors
+    rbac_policy_default      = var.argocd_rbac_policy_default
+    rbac_policy_rules        = var.argocd_rbac_policy_rules
+    enable_high_availability = var.enable_ha_argocd
+    ingress_enabled          = local.eks_exists
+    ingress_class            = "nginx-internal"
   })
 }
 
@@ -35,5 +35,5 @@ clusterAutoscaler:
     roleArn: "${local.iam_roles["cluster_autoscaler"].iam_role_arn}"
 EOF
   ]
-  depends_on = [ helm_release.argocd ]
+  depends_on = [helm_release.argocd]
 }
