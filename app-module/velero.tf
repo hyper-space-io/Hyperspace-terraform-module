3d0338f29981976d92f973b831e6c7996df866b9
Idan Brody <idan@projectcircle.co.il>
2024-11-06 10:43:57 +0200

    Installing velero

---
+++ app-module/velero.tf
@@ -0,0 +1,44 @@
+resource "helm_release" "velero" {
+  name             = "velero"
+  chart            = "velero"
+  version          = "~>7.2.1"
+  repository       = "https://vmware-tanzu.github.io/helm-charts"
+  namespace        = "velero"
+  create_namespace = true
+  values = [<<EOF
+  initContainers:
+  - name: velero-plugin-for-aws
+    image: velero/velero-plugin-for-aws:v1.10.1
+    imagePullPolicy: IfNotPresent
+    volumeMounts:
+      - mountPath: /target
+        name: plugins
+  configuration:
+    backupStorageLocation:
+      - name: "s3"
+        default: true
+        provider: "aws"
+        bucket: "${local.s3_buckets["velero"].s3_bucket_id}"
+        accessMode: "ReadWrite"
+        config: {
+          region: "${local.aws_region}"
+        }
+    volumeSnapshotLocation:
+      - name: "aws"
+        provider: "aws"
+        config: {
+          region: "${local.aws_region}"
+        }
+  podAnnotations: {
+   eks.amazonaws.com/role-arn: "${local.iam_roles["velero"].iam_role_arn}"
+  }
+  defaultBackupStorageLocation: "s3"
+  credentials:
+    useSecret: false
+  serviceAccount:
+    server:
+      annotations:
+        eks.amazonaws.com/role-arn: "${local.iam_roles["velero"].iam_role_arn}"
+  EOF
+  ]
+}
