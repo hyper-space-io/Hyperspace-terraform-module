a2749552111cb7f6287048ad1ba80fc26df4758f
Idan Brody <idan@projectcircle.co.il>
2024-11-11 16:23:16 +0200

    Removed helm release token sync

--- app-module/ecr-token.tf
+++ app-module/ecr-token.tf
@@ -1,7 +1,7 @@
-resource "helm_release" "ecr_token_sync" {
-  name            = "ecr-credentials-sync"
-  chart           = "${path.module}/ecr-credentials-sync"
-  wait            = true
-  force_update    = true
-  cleanup_on_fail = true
-}
+# resource "helm_release" "ecr_token_sync" {
+#   name            = "ecr-credentials-sync"
+#   chart           = "${path.module}/ecr-credentials-sync"
+#   wait            = true
+#   force_update    = true
+#   cleanup_on_fail = true
+# }
