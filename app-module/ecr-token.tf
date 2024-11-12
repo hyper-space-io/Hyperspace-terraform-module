7090694b4fd6f36571181a45adc36732d1a7446d
Idan Brody <idan@projectcircle.co.il>
2024-11-12 11:01:20 +0200

    Changed job namespace

--- app-module/ecr-token.tf
+++ app-module/ecr-token.tf
@@ -1,6 +1,7 @@
-resource "helm_release" "ecr_token_sync" {
+resource "helm_release" "ecr_token" {
   name            = "ecr-credentials-sync"
   chart           = "${path.module}/ecr-credentials-sync"
+  namespace       = "argocd"  
   wait            = true
   force_update    = true
   cleanup_on_fail = true
