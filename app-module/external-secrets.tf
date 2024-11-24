aff5a674d87c28ec2b8dab8b9cebf84a6228b1ad
Idan Brody <idan@projectcircle.co.il>
2024-11-24 16:24:44 +0200

    removing ecr-token

--- app-module/external-secrets.tf
+++ app-module/external-secrets.tf
@@ -1,32 +1,32 @@
-locals {
-  external_secrets_release_name = "external-secrets"
-}
-resource "helm_release" "secrets_manager" {
-  namespace        = local.external_secrets_release_name
-  chart            = local.external_secrets_release_name
-  name             = local.external_secrets_release_name
-  create_namespace = true
-  wait             = true
-  version          = "~> 0.10.5"
-  repository       = "https://charts.external-secrets.io/"
-  values           =[<<EOF
-serviceAccount:
-  annotations:
-    eks.amazonaws.com/role-arn: "${local.iam_roles["${local.external_secrets_release_name}"].iam_role_arn}"
-installCRDs: true
-EOF
-  ]
-}
+# locals {
+#   external_secrets_release_name = "external-secrets"
+# }
+# resource "helm_release" "secrets_manager" {
+#   namespace        = local.external_secrets_release_name
+#   chart            = local.external_secrets_release_name
+#   name             = local.external_secrets_release_name
+#   create_namespace = true
+#   wait             = true
+#   version          = "~> 0.10.5"
+#   repository       = "https://charts.external-secrets.io/"
+#   values           =[<<EOF
+# serviceAccount:
+#   annotations:
+#     eks.amazonaws.com/role-arn: "${local.iam_roles["${local.external_secrets_release_name}"].iam_role_arn}"
+# installCRDs: true
+# EOF
+#   ]
+# }
 
-resource "helm_release" "secret_manager_manifests" {
-  name            = "secret-manager-manifests"
-  chart           = "${path.module}/secrets-manager-manifests"
-  wait            = true
-  force_update    = true
-  cleanup_on_fail = true
-  values = [<<EOT
-  awsRegion: "${local.aws_region}"
-  EOT
-  ]
-  depends_on      = [ helm_release.secrets_manager ]
-}
+# resource "helm_release" "secret_manager_manifests" {
+#   name            = "secret-manager-manifests"
+#   chart           = "${path.module}/secrets-manager-manifests"
+#   wait            = true
+#   force_update    = true
+#   cleanup_on_fail = true
+#   values = [<<EOT
+#   awsRegion: "${local.aws_region}"
+#   EOT
+#   ]
+#   depends_on      = [ helm_release.secrets_manager ]
+# }
--- app-module/kube-prometheus-stack.tf
+++ app-module/kube-prometheus-stack.tf
@@ -1,107 +1,107 @@
-locals {
-  prometheus_release_name = "kube-prometheus-stack"
-}
-resource "helm_release" "kube_prometheus_stack" {
-  name       = local.prometheus_release_name
-  chart      = local.prometheus_release_name
-  version    = "~> 65.5.0"
-  namespace  = "monitoring"
-  repository = "https://prometheus-community.github.io/helm-charts"
-  values = [<<EOF
-global:
+# locals {
+#   prometheus_release_name = "kube-prometheus-stack"
+# }
+# resource "helm_release" "kube_prometheus_stack" {
+#   name       = local.prometheus_release_name
+#   chart      = local.prometheus_release_name
+#   version    = "~> 65.5.0"
+#   namespace  = "monitoring"
+#   repository = "https://prometheus-community.github.io/helm-charts"
+#   values = [<<EOF
+# global:
 
-  imagePullSecrets:
-    - name: "regcred-secret"
+#   imagePullSecrets:
+#     - name: "regcred-secret"
 
-commonLabels:
+# commonLabels:
 
-  environment: "${local.environment}"
+#   environment: "${local.environment}"
 
-grafana:
+# grafana:
 
-  ingress:
-    enabled: true
-    ingressClassName: "${local.internal_ingress_class_name}"
-    annotations:
-      cert-manager.io/cluster-issuer: "prod-certmanager"
-      acme.cert-manager.io/http01-edit-in-place: "true"
-      nginx.ingress.kubernetes.io/auth-type: basic
-      nginx.ingress.kubernetes.io/auth-secret: none
-    hosts:
-      - "grafana.${local.internal_domain_name}"
-    tls:
-      - secretName: "monitoring-tls"
-        hosts:
-          - "grafana.${local.internal_domain_name}"
+#   ingress:
+#     enabled: true
+#     ingressClassName: "${local.internal_ingress_class_name}"
+#     annotations:
+#       cert-manager.io/cluster-issuer: "prod-certmanager"
+#       acme.cert-manager.io/http01-edit-in-place: "true"
+#       nginx.ingress.kubernetes.io/auth-type: basic
+#       nginx.ingress.kubernetes.io/auth-secret: none
+#     hosts:
+#       - "grafana.${local.internal_domain_name}"
+#     tls:
+#       - secretName: "monitoring-tls"
+#         hosts:
+#           - "grafana.${local.internal_domain_name}"
 
-  persistence:
-    enabled: true
-    size: 10Gi
+#   persistence:
+#     enabled: true
+#     size: 10Gi
 
-additionalDataSources:
-  - name: "loki"
-    type: "loki"
-    access: "proxy"
-    url: "http://loki.monitoring.svc.cluster.local:3100"
-    version: 1
-    isDefault: false
+# additionalDataSources:
+#   - name: "loki"
+#     type: "loki"
+#     access: "proxy"
+#     url: "http://loki.monitoring.svc.cluster.local:3100"
+#     version: 1
+#     isDefault: false
 
-prometheus:
-  prometheusSpec:
-    additionalScrapeConfigs:
-      - job_name: "otel_collector"
-        scrape_interval: "10s"
-        static_configs:
-          - targets:
-            - "opentelemetry-collector.opentelemetry:9100"
-            - "opentelemetry-collector.opentelemetry:8888"
-    storageSpec:
-      volumeClaimTemplate:
-        spec:
-          resources:
-            requests:
-              storage: 100Gi
-    retention: 365d
+# prometheus:
+#   prometheusSpec:
+#     additionalScrapeConfigs:
+#       - job_name: "otel_collector"
+#         scrape_interval: "10s"
+#         static_configs:
+#           - targets:
+#             - "opentelemetry-collector.opentelemetry:9100"
+#             - "opentelemetry-collector.opentelemetry:8888"
+#     storageSpec:
+#       volumeClaimTemplate:
+#         spec:
+#           resources:
+#             requests:
+#               storage: 100Gi
+#     retention: 365d
 
-alertmanager:
-  enabled: true
+# alertmanager:
+#   enabled: true
 
-kubeEtcd:
-  enabled: false
+# kubeEtcd:
+#   enabled: false
 
-kubeControllerManager:
-  enabled: false
+# kubeControllerManager:
+#   enabled: false
 
-kubeScheduler:
-  enabled: false    
-EOF
-  ]
-  set_sensitive {
-    name  = "grafana.adminPassword"
-    value = random_password.grafana_admin_password.result
-  }
-}
+# kubeScheduler:
+#   enabled: false    
+# EOF
+#   ]
+#   set_sensitive {
+#     name  = "grafana.adminPassword"
+#     value = random_password.grafana_admin_password.result
+#   }
+# }
 
-resource "helm_release" "prometheus_adapter" {
-  name       = "prometheus-adapter"
-  version    = "~> 4.11.0"
-  namespace  = "monitoring"
-  chart      = "prometheus-adapter"
-  repository = "https://prometheus-community.github.io/helm-charts"
-  values = [<<EOF
-resources:
-  requests:
-    cpu: "10m"
-    memory: "32Mi"
-prometheus:
-  url: http://"kube-prometheus-stack-prometheus.monitoring.svc"
-EOF
-  ]
-  depends_on = [helm_release.kube_prometheus_stack]
-}
+# resource "helm_release" "prometheus_adapter" {
+#   name       = "prometheus-adapter"
+#   version    = "~> 4.11.0"
+#   namespace  = "monitoring"
+#   chart      = "prometheus-adapter"
+#   repository = "https://prometheus-community.github.io/helm-charts"
+#   values = [<<EOF
+# resources:
+#   requests:
+#     cpu: "10m"
+#     memory: "32Mi"
+# prometheus:
+#   url: http://"kube-prometheus-stack-prometheus.monitoring.svc"
+# EOF
+#   ]
+#   depends_on = [helm_release.kube_prometheus_stack]
+# }
 
-resource "random_password" "grafana_admin_password" {
-  length           = 30
-  special          = true
-  override_special = "_%@"
-}
+# resource "random_password" "grafana_admin_password" {
+#   length           = 30
+#   special          = true
+#   override_special = "_%@"
+# }
