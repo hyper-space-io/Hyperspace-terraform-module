684ae0cfd3d1da60c06bbdebf9db5e537213c3af
Idan Brody <idan@projectcircle.co.il>
2024-11-25 15:30:20 +0200

    wip

--- app-module/argocd.tf
+++ app-module/argocd.tf
@@ -9,15 +9,14 @@ locals {
     ingress_enabled          = local.eks_exists
     ingress_class            = "nginx-internal"
   })
-  argo_release_name = "argo-cd"
 }
 
 resource "helm_release" "argocd" {
   count            = var.enable_argocd ? 1 : 0
-  chart            = local.argo_release_name
+  chart            = "argo-cd"
   version          = "~> 7.7.3"
-  name             = local.argo_release_name
-  namespace        = local.argo_release_name
+  name             = "argocd"
+  namespace        = "argocd"
   create_namespace = true
   repository       = "https://argoproj.github.io/argo-helm"
   values           = [local.argocd_values]
