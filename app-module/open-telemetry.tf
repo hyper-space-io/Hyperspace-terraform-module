147fa5e6b16e451b329873b4f57e4b326a4e9a9d
Idan Brody <idan@projectcircle.co.il>
2024-11-27 17:01:01 +0200

    added otel

---
+++ app-module/open-telemetry.tf
@@ -0,0 +1,43 @@
+resource "helm_release" "opentelemetry-collector" {
+  name             = "opentelemetry-collector"
+  repository       = "https://open-telemetry.github.io/opentelemetry-helm-charts"
+  version          = "0.96.0"
+  chart            = "opentelemetry-collector"
+  namespace        = "opentelemetry"
+  create_namespace = true
+  wait             = true
+  values = [<<EOT
+mode: "deployment"
+config:
+  exporters:
+    prometheus:
+      endpoint: 0.0.0.0:9100
+      const_labels:
+        source: opentelemetry
+    debug:
+      verbosity: detailed
+  service:
+    extensions:
+      - health_check
+    pipelines:
+      metrics:
+        receivers:
+          - otlp
+        processors:
+          - batch
+        exporters:
+        - prometheus
+        - debug
+ports:
+  prometheus:
+    enabled: true
+    containerPort: 9100
+    servicePort: 9100
+    hostPort: 9100
+    protocol: TCP
+image:
+  repository: "otel/opentelemetry-collector-contrib"
+useGOMEMLIMIT: true
+EOT
+  ]
+}
