5f33180750b989ad1def384884d96472d09c209f
Idan Brody <idan@projectcircle.co.il>
2025-01-14 11:25:59 +0200

    Added workspace and agent pool

---
+++ Infra-module/app_module.tf
@@ -0,0 +1,20 @@
+resource "tfe_workspace" "app" {
+  name         = "hyperspace-app-module"
+  organization = data.tfe_organizations.foo.name
+}
+
+resource "tfe_agent_pool" "app-agent-pool" {
+  name         = "hyperspace-app-agent-pool"
+  organization = data.tfe_organizations.foo.name
+}
+
+resource "tfe_agent_pool_allowed_workspaces" "app" {
+  agent_pool_id         = tfe_agent_pool.app-agent-pool.id
+  allowed_workspace_ids = [tfe_workspace.app.id]
+}
+
+resource "tfe_workspace_settings" "app-settings" {
+  workspace_id   = tfe_workspace.app.id
+  agent_pool_id  = tfe_agent_pool_allowed_workspaces.app.agent_pool_id
+  execution_mode = "agent"
+}
