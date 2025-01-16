da5968a24e6c8eac0e6033b656da95d0d5d33b86
Idan Brody <idan@projectcircle.co.il>
2025-01-16 12:15:59 +0200

    Added json decode in argocd connectors

--- app-module/argocd.tf
+++ app-module/argocd.tf
@@ -2,7 +2,7 @@ locals {
   argocd_values = templatefile("${path.module}/argocd_values.tftpl", {
     dex_enabled              = length(var.dex_connectors) > 0
     domain                   = local.internal_domain_name
-    dex_connectors           = var.dex_connectors
+    dex_connectors           = jsondecode(var.dex_connectors)
     rbac_policy_default      = var.argocd_rbac_policy_default
     rbac_policy_rules        = var.argocd_rbac_policy_rules
     enable_high_availability = var.enable_ha_argocd
