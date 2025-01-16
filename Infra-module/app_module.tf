23362c3f4da85e3025ca979c80938267661b3962
Idan Brody <idan@projectcircle.co.il>
2025-01-16 10:57:59 +0200

    Added agent token

--- Infra-module/app_module.tf
+++ Infra-module/app_module.tf
@@ -10,10 +10,15 @@ resource "tfe_workspace" "app" {
   working_directory = "app-module"
 }
 
+resource "tfe_workspace_settings" "app-settings" {
+  workspace_id   = tfe_workspace.app.id
+  agent_pool_id  = tfe_agent_pool_allowed_workspaces.app.agent_pool_id
+  execution_mode = "agent"
+}
+
 resource "tfe_agent_pool" "app-agent-pool" {
   name         = "hyperspace-app-agent-pool"
   organization = data.tfe_organizations.all.names[0]
-
 }
 
 resource "tfe_agent_pool_allowed_workspaces" "app" {
@@ -21,8 +26,7 @@ resource "tfe_agent_pool_allowed_workspaces" "app" {
   allowed_workspace_ids = [tfe_workspace.app.id]
 }
 
-resource "tfe_workspace_settings" "app-settings" {
-  workspace_id   = tfe_workspace.app.id
-  agent_pool_id  = tfe_agent_pool_allowed_workspaces.app.agent_pool_id
-  execution_mode = "agent"
+resource "tfe_agent_token" "app-agent-token" {
+  agent_pool_id = tfe_agent_pool.app-agent-pool.id
+  description   = "app-agent-token"
 }
