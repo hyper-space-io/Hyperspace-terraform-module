b7d90d3e9d841c34aecee7a65f1c2dedf3ca09bf
Idan Brody <idan@projectcircle.co.il>
2025-01-19 14:59:43 +0200

    Revert "typo"
    This reverts commit 1aad234ee2e9f45619e64b852b454906a46a5f94.

--- Infra-module/main.tf
+++ Infra-module/main.tf
@@ -6,3 +6,31 @@ resource "aws_iam_policy" "policies" {
   description = each.value.description
   policy      = each.value.policy
 }
+
+module "iam_iam-assumable-role-with-oidc" {
+  source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version                       = "~> 5.48.0"
+  for_each                      = { for k, v in local.iam_policies : k => v if lookup(v, "create_assumable_role", false) == true }
+  create_role                   = true
+  role_name                     = each.value.name
+  provider_url                  = module.eks.cluster_oidc_issuer_url
+  role_policy_arns              = [aws_iam_policy.policies["${each.key}"].arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:${each.value.sa_namespace}:${each.key}"]
+}
+
+module "boto3_irsa" {
+  source    = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  for_each  = { for k, v in local.iam_policies : k => v if lookup(v, "create_cluster_wide_role", false) == true }
+  role_name = each.value.name
+  role_policy_arns = {
+    policy = aws_iam_policy.policies["${each.key}"].arn
+  }
+  assume_role_condition_test = "StringLike"
+  oidc_providers = {
+    ex = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["*:*"]
+    }
+  }
+  depends_on = [module.eks, aws_iam_policy.policies]
+}
