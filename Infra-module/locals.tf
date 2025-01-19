7b58bb18d7a0c876b68b1f1914ee633c1a06d03f
Idan Brody <idan@projectcircle.co.il>
2025-01-19 14:59:44 +0200

    Revert "wip"
    This reverts commit e971c4ea0ef7310aceb92790ac75d99523c96e52.

--- Infra-module/locals.tf
+++ Infra-module/locals.tf
@@ -81,8 +81,8 @@ locals {
     }
   ]...)
 
-  # self_managed_node_groups = { after_apply = local.self_managed_node_groups_local }
-  # precomputed_self_managed_node_groups = var.create_eks ? local.self_managed_node_groups["after_apply"] : {}
+  self_managed_node_groups = { after_apply = local.self_managed_node_groups_local }
+  precomputed_self_managed_node_groups = var.create_eks ? local.self_managed_node_groups["after_apply"] : {}
 
   ##################
   #  IAM POLICIES  #
