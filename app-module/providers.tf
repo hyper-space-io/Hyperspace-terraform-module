a844e6446ab6d4e57dfd124d0aa2d7f8bf4529e7
Idan Brody <idan@projectcircle.co.il>
2025-01-23 16:43:40 +0200

    wip

--- app-module/providers.tf
+++ app-module/providers.tf
@@ -1,7 +1,11 @@
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.eks.token
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", local.cluster_name]
+    command     = "aws"
+  }
 }
 
 provider "helm" {
