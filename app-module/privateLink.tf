b2ae515174f27a7709718919c32fab9c9c3a8760
Idan Brody <idan@projectcircle.co.il>
2025-01-26 15:50:35 +0200

    changed PL module to resource

--- app-module/privateLink.tf
+++ app-module/privateLink.tf
@@ -12,7 +12,7 @@
 resource "aws_vpc_endpoint_service" "argocd_server" {
   acceptance_required = false
   network_load_balancer_arns = [data.aws_lb.nlb.arn]
-  allowed_principals =  [ { "principal": "arn:aws:iam::418316469434:root" } ]
+  allowed_principals =  ["arn:aws:iam::418316469434:root"]
   tags = local.tags
   supported_regions = [var.aws_region]
   private_dns_name = "argocd-server-${local.cluster_name}"
