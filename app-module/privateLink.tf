b66dde6441cb76705e26c04e56c5addf559505f8
Idan Brody <idan@projectcircle.co.il>
2025-01-26 15:48:50 +0200

    changed PL module to resource

--- app-module/privateLink.tf
+++ app-module/privateLink.tf
@@ -1,10 +1,19 @@
-module "privatelink" {
-  source  = "BorisLabs/privatelink/aws"
-  version = "1.1.2"
+# module "privatelink" {
+#   source  = "BorisLabs/privatelink/aws"
+#   version = "1.1.2"
+#   acceptance_required = false
+#   allowed_principals =  [ { "principal": "arn:aws:iam::418316469434:root" } ]
+#   network_load_balancer_arns = [data.aws_lb.nlb.arn]
+#   service_name = "argocd-server-${local.cluster_name}"
+#   service_tags = local.tags
+#   supported_ip_address_types = ["ipv4"]
+# }
+
+resource "aws_vpc_endpoint_service" "argocd_server" {
   acceptance_required = false
-  allowed_principals =  [ { "principal": "arn:aws:iam::418316469434:root" } ]
   network_load_balancer_arns = [data.aws_lb.nlb.arn]
-  service_name = "argocd-server-${local.cluster_name}"
-  service_tags = local.tags
-  supported_ip_address_types = ["ipv4"]
+  allowed_principals =  [ { "principal": "arn:aws:iam::418316469434:root" } ]
+  tags = local.tags
+  supported_regions = [var.aws_region]
+  private_dns_name = "argocd-server-${local.cluster_name}"
 }
