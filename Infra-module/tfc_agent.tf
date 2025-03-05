5966398e84baa051a34b1e13c098b1a1dfb5bfb9
arielrahamim <ariel_rahamim@yahoo.com>
2025-03-05 17:07:45 +0200

    Recreate TFC Agent2

--- Infra-module/tfc_agent.tf
+++ Infra-module/tfc_agent.tf
@@ -1,27 +1,27 @@
 # TFC AGENT
-# resource "aws_instance" "tfc_agent" {
-#   ebs_optimized          = true
-#   monitoring             = true
-#   instance_type          = "t3.medium"
-#   ami                    = data.aws_ami.amazon_linux_2.id
-#   subnet_id              = module.vpc.private_subnets[0]
-#   iam_instance_profile   = aws_iam_instance_profile.tfc_agent_profile.name
-#   vpc_security_group_ids = [aws_security_group.tfc_agent_sg.id]
-#   user_data = templatefile("${path.module}/user_data.sh.tpl", {
-#     tfc_agent_token = tfe_agent_token.app-agent-token.token
-#   })
-#   tags = {
-#     Name = "tfc-agent"
-#   }
-#   root_block_device {
-#     volume_type           = "gp3"
-#     volume_size           = 20
-#     delete_on_termination = true
-#   }
-#   metadata_options {
-#     http_tokens = "required"
-#   }
-# }
+resource "aws_instance" "tfc_agent" {
+  ebs_optimized          = true
+  monitoring             = true
+  instance_type          = "t3.medium"
+  ami                    = data.aws_ami.amazon_linux_2.id
+  subnet_id              = module.vpc.private_subnets[0]
+  iam_instance_profile   = aws_iam_instance_profile.tfc_agent_profile.name
+  vpc_security_group_ids = [aws_security_group.tfc_agent_sg.id]
+  user_data = templatefile("${path.module}/user_data.sh.tpl", {
+    tfc_agent_token = tfe_agent_token.app-agent-token.token
+  })
+  tags = {
+    Name = "tfc-agent"
+  }
+  root_block_device {
+    volume_type           = "gp3"
+    volume_size           = 20
+    delete_on_termination = true
+  }
+  metadata_options {
+    http_tokens = "required"
+  }
+}
 
 
 resource "aws_iam_role" "tfc_agent_role" {
