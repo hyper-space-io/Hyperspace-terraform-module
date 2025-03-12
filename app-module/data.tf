6b0036150d005e178449553abc7d1fc37cb699df
arielrahamim <ariel@projectcircle.co.il>
2025-03-12 12:44:54 +0200

    WIP

--- app-module/data.tf
+++ app-module/data.tf
@@ -22,21 +22,21 @@ data "kubernetes_storage_class" "name" {
   depends_on = [module.eks]
 }
 
-# data "kubernetes_ingress_v1" "external_ingress" {
-#   metadata {
-#     name      = "external-ingress"
-#     namespace = "ingress"
-#   }
-#   depends_on = [time_sleep.wait_for_external_ingress, module.eks, kubernetes_ingress_v1.nginx_ingress]
-# }
+data "kubernetes_ingress_v1" "external_ingress" {
+  metadata {
+    name      = "external-ingress"
+    namespace = "ingress"
+  }
+  depends_on = [time_sleep.wait_for_external_ingress, module.eks, kubernetes_ingress_v1.nginx_ingress]
+}
 
-# data "kubernetes_ingress_v1" "internal_ingress" {
-#   metadata {
-#     name      = "internal-ingress"
-#     namespace = "ingress"
-#   }
-#   depends_on = [time_sleep.wait_for_internal_ingress, module.eks, kubernetes_ingress_v1.nginx_ingress]
-# }
+data "kubernetes_ingress_v1" "internal_ingress" {
+  metadata {
+    name      = "internal-ingress"
+    namespace = "ingress"
+  }
+  depends_on = [time_sleep.wait_for_internal_ingress, module.eks, kubernetes_ingress_v1.nginx_ingress]
+}
 
 data "aws_eks_cluster_auth" "eks" {
   name       = local.cluster_name
@@ -52,10 +52,10 @@ data "aws_ami" "fpga" {
 ### Load Balancer #####
 #######################
 
-# data "aws_lb" "nlb" {
-#   tags = {
-#     "service.k8s.aws/stack" = "argocd/argocd-server"
-#     "elbv2.k8s.aws/cluster" = module.eks.cluster_name
-#   }
-#   depends_on = [helm_release.argocd]
-# }
+data "aws_lb" "nlb" {
+  tags = {
+    "service.k8s.aws/stack" = "argocd/argocd-server"
+    "elbv2.k8s.aws/cluster" = module.eks.cluster_name
+  }
+  depends_on = [helm_release.argocd]
+}
