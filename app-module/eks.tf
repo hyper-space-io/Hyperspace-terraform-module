94476087a61f8e8991096717244cdae35a219c7e
arielrahamim <ariel@projectcircle.co.il>
2025-03-12 12:23:31 +0200

    Disable LB

--- app-module/eks.tf
+++ app-module/eks.tf
@@ -229,7 +229,7 @@ module "eks_blueprints_addons" {
   cluster_endpoint                    = module.eks.cluster_endpoint
   cluster_version                     = module.eks.cluster_version
   oidc_provider_arn                   = module.eks.oidc_provider_arn
-  enable_aws_load_balancer_controller = true
+  enable_aws_load_balancer_controller = false
   aws_load_balancer_controller        = { values = [local.alb_values], wait = true }
 }
 
