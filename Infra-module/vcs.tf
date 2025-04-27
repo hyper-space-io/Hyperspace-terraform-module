7872e68d3e889a792bb7f2c1be17da03123d11bf
arielrahamim <ariel@projectcircle.co.il>
2025-04-27 16:55:27 +0300

    WIP

--- Infra-module/vcs.tf
+++ Infra-module/vcs.tf
@@ -1,29 +1,29 @@
-resource "null_resource" "vcs_repo_reattach" {
-  triggers = {
-    # Trigger on any change to the workspace or VCS configuration
-    timestamp    = timestamp()
-  }
+# resource "null_resource" "vcs_repo_reattach" {
+#   triggers = {
+#     # Trigger on any change to the workspace or VCS configuration
+#     timestamp    = timestamp()
+#   }
 
-  provisioner "local-exec" {
-    command = <<-EOT
-      # Detach VCS repo
-      curl -s -H "Authorization: Bearer ${var.tfc_agent_token}" \
-        -H "Content-Type: application/vnd.api+json" \
-        -X PATCH \
-        "https://app.terraform.io/api/v2/workspaces/${tfe_workspace.app.id}" \
-        -d '{"data":{"type":"workspaces","attributes":{"vcs-repo":null}}}'
+#   provisioner "local-exec" {
+#     command = <<-EOT
+#       # Detach VCS repo
+#       curl -s -H "Authorization: Bearer ${var.tfc_agent_token}" \
+#         -H "Content-Type: application/vnd.api+json" \
+#         -X PATCH \
+#         "https://app.terraform.io/api/v2/workspaces/${tfe_workspace.app.id}" \
+#         -d '{"data":{"type":"workspaces","attributes":{"vcs-repo":null}}}'
 
-      # Wait a moment for the detach to complete
-      sleep 5
+#       # Wait a moment for the detach to complete
+#       sleep 5
 
-      # Reattach VCS repo
-      curl -s -H "Authorization: Bearer ${var.tfc_agent_token}" \
-        -H "Content-Type: application/vnd.api+json" \
-        -X PATCH \
-        "https://app.terraform.io/api/v2/workspaces/${tfe_workspace.app.id}" \
-        -d '{"data":{"type":"workspaces","attributes":{"vcs-repo":{"identifier":"${local.hyperspace_org_name}/Hyperspace-terraform-module","branch":"${var.argocd_vcs_configuration.branch}","oauth-token-id":null,"github-app-installation-id":null}}}}'
-    EOT
-  }
+#       # Reattach VCS repo
+#       curl -s -H "Authorization: Bearer ${var.tfc_agent_token}" \
+#         -H "Content-Type: application/vnd.api+json" \
+#         -X PATCH \
+#         "https://app.terraform.io/api/v2/workspaces/${tfe_workspace.app.id}" \
+#         -d '{"data":{"type":"workspaces","attributes":{"vcs-repo":{"identifier":"${local.hyperspace_org_name}/Hyperspace-terraform-module","branch":"${var.argocd_vcs_configuration.branch}","oauth-token-id":null,"github-app-installation-id":null}}}}'
+#     EOT
+#   }
 
-  depends_on = [tfe_workspace.app]
-} 
+#   depends_on = [tfe_workspace.app]
+# }
