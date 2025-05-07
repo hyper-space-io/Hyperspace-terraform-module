ef8c40851a195c54896f9d547bf9035fa8fe38b0
arielrahamim <ariel_rahamim@yahoo.com>
2025-05-07 12:47:50 +0300

    seperated acm module to two to not use for each to resolve sensitive acm for each error

--- acm.tf
+++ acm.tf
@@ -1,38 +1,33 @@
 locals {
   create_acm = var.domain_name != "" ? true : false
-  
-  # Create a list of certificate configurations
-  certificate_configs = [
-    {
-      name = "external"
-      enabled = var.create_public_zone
-      domain_name = local.public_domain_name
-      subject_alternative_names = [
-        "*.${local.public_domain_name}",
-      ]
-      create_certificate = local.create_acm
-    },
-    {
-      name = "internal"
-      enabled = true
-      domain_name = local.internal_domain_name
-      subject_alternative_names = [
-        "*.${local.internal_domain_name}",
-      ]
-      create_certificate = local.create_acm
-    }
-  ]
 }
 
-module "acm" {
-  source                    = "terraform-aws-modules/acm/aws"
-  version                   = "~> 5.1.1"
-  for_each                  = { for cert in local.certificate_configs : cert.name => cert if cert.enabled && cert.domain_name != "" }
-  create_certificate        = each.value.create_certificate
-  domain_name               = each.value.domain_name
-  subject_alternative_names = each.value.subject_alternative_names
-  tags                      = local.tags
-  create_route53_records    = false
-  validation_method         = "DNS"
-  wait_for_validation       = true
+module "external_acm" {
+  count                       = var.create_public_zone && local.public_domain_name != "" ? 1 : 0
+  source                      = "terraform-aws-modules/acm/aws"
+  version                     = "~> 5.1.1"
+  create_certificate          = local.create_acm
+  domain_name                 = local.public_domain_name
+  subject_alternative_names   = [
+    "*.${local.public_domain_name}",
+  ]
+  tags                        = local.tags
+  create_route53_records      = false
+  validation_method           = "DNS"
+  wait_for_validation         = true
+}
+
+module "internal_acm" {
+  count                       = local.internal_domain_name != "" ? 1 : 0
+  source                      = "terraform-aws-modules/acm/aws"
+  version                     = "~> 5.1.1"
+  create_certificate          = local.create_acm
+  domain_name                 = local.internal_domain_name
+  subject_alternative_names   = [
+    "*.${local.internal_domain_name}",
+  ]
+  tags                        = local.tags
+  create_route53_records      = false
+  validation_method           = "DNS"
+  wait_for_validation         = true
 }
