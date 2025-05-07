310354a63acdf0cd6b42159bbc15bcec8b977550
arielrahamim <ariel_rahamim@yahoo.com>
2025-05-07 17:13:18 +0300

    WIP

--- providers.tf
+++ providers.tf
@@ -35,18 +35,6 @@ terraform {
       source  = "gavinbunney/kubectl"
       version = "~> 1.14.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.0"
-    }
-    time = {
-      source  = "hashicorp/time"
-      version = "~> 0.9.0"
-    }
   }
 }
 
