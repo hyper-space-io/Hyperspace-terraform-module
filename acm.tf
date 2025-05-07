7a6213054196866fafc9322c63a435e778098816
arielrahamim <ariel_rahamim@yahoo.com>
2025-05-07 12:43:17 +0300

    testing sensitive acm for each error

--- acm.tf
+++ acm.tf
@@ -1,27 +1,35 @@
 locals {
   create_acm = var.domain_name != "" ? true : false
-  acm_config = {
-    external_domains = (var.create_public_zone && local.public_domain_name != "") ? {
-      domain_name = local.public_domain_name
-      subject_alternative_names = [
-        "*.${local.public_domain_name}",
-      ]
-      create_certificate = local.create_acm
-    } : null,
-    internal_domains = local.internal_domain_name != "" ? {
-      domain_name = local.internal_domain_name
-      subject_alternative_names = [
-        "*.${local.internal_domain_name}",
-      ]
-      create_certificate = local.create_acm
-    } : null
-  }
+  
+  # Create a list of certificate configurations
+  certificate_configs = [
+    {
+      name = "external"
+      config = (var.create_public_zone && local.public_domain_name != "") ? {
+        domain_name = local.public_domain_name
+        subject_alternative_names = [
+          "*.${local.public_domain_name}",
+        ]
+        create_certificate = local.create_acm
+      } : null
+    },
+    {
+      name = "internal"
+      config = local.internal_domain_name != "" ? {
+        domain_name = local.internal_domain_name
+        subject_alternative_names = [
+          "*.${local.internal_domain_name}",
+        ]
+        create_certificate = local.create_acm
+      } : null
+    }
+  ]
 }
 
 module "acm" {
   source                    = "terraform-aws-modules/acm/aws"
   version                   = "~> 5.1.1"
-  for_each                  = { for k, v in local.acm_config : k => v if v != null }
+  for_each                  = { for cert in local.certificate_configs : cert.name => cert.config if cert.config != null }
   create_certificate        = each.value.create_certificate
   domain_name               = each.value.domain_name
   subject_alternative_names = each.value.subject_alternative_names
