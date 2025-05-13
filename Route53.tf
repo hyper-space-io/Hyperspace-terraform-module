6a9ddb344b3dc6c1202d6d04d582fc7456185108
arielrahamim <ariel@projectcircle.co.il>
2025-05-13 10:47:07 +0300

    Removed dependson from route53 zones creation

--- Route53.tf
+++ Route53.tf
@@ -17,7 +17,6 @@ module "external_zone" {
       })
     }
   }
-  depends_on = [module.external_acm]
 }
 
 module "internal_zone" {
@@ -38,7 +37,6 @@ module "internal_zone" {
       })
     }
   }
-  depends_on = [module.internal_acm]
 }
 
 resource "aws_route53_record" "wildcard" {
