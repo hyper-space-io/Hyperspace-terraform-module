edc80f6fb7a68c927d8e6feea5723934c8d54b6b
arielrahamim <ariel@projectcircle.co.il>
2025-05-20 11:35:41 +0300

    WIP

---
+++ ecr-token.tf
@@ -0,0 +1,16 @@
+resource "helm_release" "ecr_token" {
+  count           = var.create_eks ? 1 : 0
+  name            = "ecr-credentials-sync"
+  chart           = "${path.module}/ecr-credentials-sync"
+  namespace       = "argocd"
+  wait            = true
+  force_update    = true
+  cleanup_on_fail = true
+  depends_on      = [module.eks, time_sleep.wait_for_cluster_ready, helm_release.argocd]
+
+  values = [<<EOT
+    account_id   = "${var.hyperspace_account_id}"
+    ECR_REGISTRY = "${var.hyperspace_account_id}.dkr.ecr.eu-west-1.amazonaws.com"
+  EOT
+  ]
+}
