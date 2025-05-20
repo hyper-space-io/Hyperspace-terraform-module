a87a175ae3ffe3f2aaa272465e757903069e181e
arielrahamim <ariel@projectcircle.co.il>
2025-05-20 13:52:05 +0300

    WIP

--- hyperspace.tf
+++ hyperspace.tf
@@ -6,35 +6,38 @@ resource "helm_release" "hyperspace" {
   force_update    = true
   cleanup_on_fail = true
   
-  set {
-    name  = "awsRegion"
-    value = var.aws_region
-  }
-
-  set {
-    name  = "global.environment"
-    value = var.environment
-  }
-
-  set {
-    name  = "global.ecr_repository"
-    value = "${var.aws_account_id}.dkr.ecr.${var.aws_region}.amazonaws.com"
-  }
-
-  set {
-    name  = "data-node.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = "arn:aws:iam::${var.aws_account_id}:role/${var.project}-${var.environment}-EC2TagsPolicy"
-  }
-
-  set {
-    name  = "search-master.ingress.hosts[0].host"
-    value = "search-master.${var.environment}.${var.domain_name}"
-  }
-
-  set {
-    name  = "etcd.fullnameOverride"
-    value = "etcd-${var.environment}"
-  }
-
+  values = [
+  yamlencode({
+    global = {
+      environment   = var.environment
+      ecr_repository = "${var.aws_account_id}.dkr.ecr.${var.aws_region}.amazonaws.com"
+    }
+    "data-node" = {
+      serviceAccount = {
+        annotations = {
+          "eks.amazonaws.com/role-arn" = "arn:aws:iam::${var.aws_account_id}:role/${var.project}-${var.environment}-EC2TagsPolicy"
+        }
+      }
+    }
+    "search-master" = {
+      ingress = {
+        hosts = [
+          {
+            host = "search-master.${var.environment}.${var.domain_name}"
+            paths = [
+              {
+                path     = "/"
+                pathType = "Prefix"
+              }
+            ]
+          }
+        ]
+      }
+    }
+    etcd = {
+      fullnameOverride = "etcd-${var.environment}"
+    }
+  })
+]
   depends_on = [helm_release.secrets_manager, time_sleep.wait_for_crd]
 }
