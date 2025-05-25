4bc5507ac24c7dcc851e2e4b3e107dda0bb496d2
arielrahamim <ariel@projectcircle.co.il>
2025-05-25 13:25:29 +0300

    aws provider ver

--- providers.tf
+++ providers.tf
@@ -21,7 +21,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.98.0"
+      version = "~> 5.90.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
