fc43acd38df23c4ed8e969f393ed42f5b0060aa6
arielrahamim <ariel@projectcircle.co.il>
2025-05-25 12:05:54 +0300

    remove create_cert from acm module

--- acm.tf
+++ acm.tf
@@ -1,13 +1,11 @@
 locals {
-  create_acm             = var.domain_name != "" ? true : false
-  create_route53_records = local.create_acm && local.validation_zone_id != null ? true : false
+  create_route53_records = local.validation_zone_id != null ? true : false
 }
 
 module "external_acm" {
   count              = (local.public_domain_name != "" && (var.create_public_zone || var.existing_public_zone_id != "")) ? 1 : 0
   source             = "terraform-aws-modules/acm/aws"
   version            = "~> 5.1.1"
-  create_certificate = local.create_acm
   domain_name        = local.public_domain_name
   subject_alternative_names = [
     "*.${local.public_domain_name}",
@@ -23,7 +21,6 @@ module "internal_acm" {
   count              = (local.internal_domain_name != "" && local.create_private_zone) ? 1 : 0
   source             = "terraform-aws-modules/acm/aws"
   version            = "~> 5.1.1"
-  create_certificate = local.create_acm
   domain_name        = local.internal_domain_name
   subject_alternative_names = [
     "*.${local.internal_domain_name}",
