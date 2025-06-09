f6879d34ffcec195f14f26f842bce08e45482fb5
Idan Brody <idan@projectcircle.co.il>
2025-06-09 15:30:00 +0300

    Fixed github access to argocd

--- locals.tf
+++ locals.tf
@@ -278,7 +278,7 @@ locals {
 
   # SSO access rules
   sso_rbac_rules = try(local.argocd_config.rbac.sso_admin_group != "", false) ? [
-    "g, ${try(local.argocd_config.rbac.sso_admin_group, "")}, role:org-admin",
+    "g, ${try(local.argocd_config.vcs.organization, "")}:${try(local.argocd_config.rbac.sso_admin_group, "")}, role:org-admin",
     "g, ${try(local.argocd_config.vcs.organization, "")}:*, role:org-admin"
     ] : [
     "g, ${try(local.argocd_config.vcs.organization, "")}:*, role:org-admin",
