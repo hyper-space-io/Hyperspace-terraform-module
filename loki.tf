331607016fc2bc44bb98646b060cde99f2e293b2
Idan Brody <idan@projectcircle.co.il>
2025-06-18 14:37:49 +0300

    wip

--- loki.tf
+++ loki.tf
@@ -5,8 +5,8 @@ resource "helm_release" "loki" {
   create_namespace = true
   cleanup_on_fail  = true
   repository       = "https://grafana.github.io/helm-charts"
-  chart            = "loki"
-  version          = "~> 6.30.1"
+  chart            = "loki-stack"
+  version          = "~> 2.10.2"
   wait             = true
   values = [<<EOF
 loki:
@@ -16,7 +16,10 @@ loki:
     annotations:
       eks.amazonaws.com/role-arn: "${module.iam_iam-assumable-role-with-oidc["loki"].iam_role_arn}"
 
-  structuredConfig:
+  extraArgs:
+    target: all,table-manager
+
+  config:
     schema_config:
       configs:
         - from: "2024-01-01"
@@ -26,6 +29,7 @@ loki:
           index:
             prefix: "${local.cluster_name}-loki-index-"
             period: "8904h"
+
     storage_config:
       aws:
         s3: "s3://${var.aws_region}/${local.s3_bucket_names["loki"]}"
@@ -34,14 +38,23 @@ loki:
         region: "${var.aws_region}"
         insecure: false
         sse_encryption: true
+
         dynamodb:
           dynamodb_url: "dynamodb://${var.aws_region}"
+    
     table_manager:
       retention_deletes_enabled: true
       retention_period: "8904h"
       index_tables_provisioning:
         enable_ondemand_throughput_mode: true
         enable_inactive_throughput_on_demand_mode: true
+    
+promtail:
+  tolerations:
+  - key: "fpga"
+    operator: "Equal"
+    value: "true"
+    effect: "NoSchedule"
 EOF
   ]
   depends_on = [module.eks, module.iam_iam-assumable-role-with-oidc]
