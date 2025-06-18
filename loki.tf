bb1a6e3169f4afdd68af9dc4702362767a8cded7
Idan Brody <idan@projectcircle.co.il>
2025-06-18 11:58:28 +0300

    modified loki values

--- loki.tf
+++ loki.tf
@@ -16,10 +16,7 @@ loki:
     annotations:
       eks.amazonaws.com/role-arn: "${module.iam_iam-assumable-role-with-oidc["loki"].iam_role_arn}"
 
-  extraArgs:
-    target: all,table-manager
-
-  config:
+  structuredConfig:
     schema_config:
       configs:
         - from: "2024-01-01"
@@ -29,7 +26,6 @@ loki:
           index:
             prefix: "${local.cluster_name}-loki-index-"
             period: "8904h"
-
     storage_config:
       aws:
         s3: "s3://${var.aws_region}/${local.s3_bucket_names["loki"]}"
@@ -38,23 +34,14 @@ loki:
         region: "${var.aws_region}"
         insecure: false
         sse_encryption: true
-
         dynamodb:
           dynamodb_url: "dynamodb://${var.aws_region}"
-    
     table_manager:
       retention_deletes_enabled: true
       retention_period: "8904h"
       index_tables_provisioning:
         enable_ondemand_throughput_mode: true
         enable_inactive_throughput_on_demand_mode: true
-    
-promtail:
-  tolerations:
-  - key: "fpga"
-    operator: "Equal"
-    value: "true"
-    effect: "NoSchedule"
 EOF
   ]
   depends_on = [module.eks, module.iam_iam-assumable-role-with-oidc]
