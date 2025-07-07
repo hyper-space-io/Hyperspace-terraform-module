fc51eff388597ee536239dfa358ae11ec6c40884
Idan Brody <idan@projectcircle.co.il>
2025-07-07 11:25:59 +0300

    Changed provider version for aws

--- providers.tf
+++ providers.tf
@@ -21,7 +21,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.90.0"
+      version = "~> 5.95.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
